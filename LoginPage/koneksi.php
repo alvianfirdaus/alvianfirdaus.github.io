9b3b78d7c199863839665c6413f914b35a7cef4a
= <alvianfirdaus896@gmail.com>
2023-06-28 03:15:42 +0700

    tessse

--- LoginPage/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "sistem_alumni";
-
-    $connect = mysqli_connect($hostname, $username, $password, $database);
-    
-?>
